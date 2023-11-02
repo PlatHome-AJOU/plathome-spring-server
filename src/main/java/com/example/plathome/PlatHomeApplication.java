d60e207076a247996c6724f5f7dece7bb8010112
Yang JiWoong <jerry3269@ajou.ac.kr>
2023-11-02 09:16:08 +0900

    initial project setting

---
+++ src/main/java/com/example/plathome/PlatHomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.plathome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/plathome/PlatHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plathome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
