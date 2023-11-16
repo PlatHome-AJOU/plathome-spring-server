1576e6e8faef7567c983fbb36d12ec29e73fdabc
Yang JiWoong <jerry3269@ajou.ac.kr>
2023-11-16 14:00:30 +0900

    #15 - spring과 redis 연동
    RefreshToken과 AuthCode 저장을 위한 redis 연동

--- src/main/java/com/example/plathome/login/member/domain/RefreshToken.java
+++
@@ -1,7 +0,0 @@
-package com.example.plathome.login.member.domain;
-
-public record RefreshToken(String refreshToken) {
-    public static RefreshToken of(String refreshToken){
-        return new RefreshToken(refreshToken);
-    }
-}
