0cfc69bd909a9c21dac4b676a74892ea662e995c
Yang JiWoong <jerry3269@ajou.ac.kr>
2023-11-25 19:48:07 +0900

    토큰 헤더명 변경

--- src/main/java/com/example/plathome/login/jwt/common/JwtStaticField.java
+++ src/main/java/com/example/plathome/login/jwt/common/JwtStaticField.java
@@ -3,8 +3,8 @@ package com.example.plathome.login.jwt.common;
 public class JwtStaticField {
     public static final String BEARER = "Bearer ";
     public static final String REFRESH_URL = "/token";
-    public static final String ACCESS_HEADER = "x-access-token";
-    public static final String REFRESH_HEADER = "x-refresh-token";
+    public static final String ACCESS_HEADER = "X-Access-Token";
+    public static final String REFRESH_HEADER = "X-Refresh-Token";
     public static final long ACCESS_TOKEN_EXPIRATION = 1000L * 60 * 30; // Access token is 30 minutes.
     public static final long REFRESH_TOKEN_EXPIRATION = 1000L * 60 * 60 * 24; // Refresh token is one day.
 
