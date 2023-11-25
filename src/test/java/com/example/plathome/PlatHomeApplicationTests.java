06c950bff4c9904880278855756538bf5b7be546
Yang JiWoong <jerry3269@ajou.ac.kr>
2023-11-25 14:50:47 +0900

    #43 - TEST 임시 비활성화

--- src/test/java/com/example/plathome/PlatHomeApplicationTests.java
+++ src/test/java/com/example/plathome/PlatHomeApplicationTests.java
@@ -1,15 +1,15 @@
-package com.example.plathome;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-@ActiveProfiles("test")
-@SpringBootTest
-class PlatHomeApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.example.plathome;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ActiveProfiles;
+//
+//@ActiveProfiles("test")
+//@SpringBootTest
+//class PlatHomeApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
