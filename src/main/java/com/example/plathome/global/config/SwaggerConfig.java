c056c44bf96c418c7c4e6277f1cdc37365e1b683
Yang JiWoong <jerry3269@ajou.ac.kr>
2023-11-26 14:23:30 +0900

    gitflow-feature-stash: 47-cors-and-swagger-ui-token

---
+++ src/main/java/com/example/plathome/global/config/SwaggerConfig.java
@@ -0,0 +1,51 @@
+package com.example.plathome.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+import static com.example.plathome.login.jwt.common.JwtStaticField.ACCESS_HEADER;
+import static com.example.plathome.login.jwt.common.JwtStaticField.REFRESH_HEADER;
+
+@OpenAPIDefinition(
+        info = @Info(title = "PlatHome API 명세서",
+                description = "User-Service API 명세서",
+                version = "v1"))
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+        SecurityScheme accessTokenScheme = new SecurityScheme()
+                .name(ACCESS_HEADER)
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        SecurityScheme refreshTokenScheme = new SecurityScheme()
+                .name(REFRESH_HEADER)
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("accessToken")
+                .addList("refreshToken");
+
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("accessToken", accessTokenScheme)
+                        .addSecuritySchemes("refreshToken", refreshTokenScheme))
+                .security(Arrays.asList(securityRequirement));
+    }
+
+}
--- src/main/java/com/example/plathome/global/config/WebConfig.java
+++ src/main/java/com/example/plathome/global/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.example.plathome.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,8 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
-                .allowCredentials(true);
+                .allowedMethods("*")
+                .allowCredentials(true)
+                .allowedOriginPatterns("*");
     }
 }
