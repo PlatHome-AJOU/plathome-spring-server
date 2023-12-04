f4c280a527f6443a5ab4e7fd9b63a12b7b716a4d
Yang JiWoong <jerry3269@ajou.ac.kr>
2023-12-04 14:00:57 +0900

    gitflow-feature-stash: 56-asdf

--- src/test/java/com/example/plathome/service/RequestedServiceTest.java
+++ src/test/java/com/example/plathome/service/RequestedServiceTest.java
@@ -1,7 +1,24 @@
 package com.example.plathome.service;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+import com.example.plathome.estate.requested.repository.RequestedRepository;
+import com.example.plathome.estate.requested.repository.ThumbNailRepository;
+import com.example.plathome.estate.requested.service.RequestedService;
+import com.example.plathome.global.service.S3Service;
+import com.example.plathome.member.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@DisplayName("비니지스 로직 - 등록 요청 매물")
+@ExtendWith(MockitoExtension.class)
 class RequestedServiceTest {
+    @InjectMocks private RequestedService sut;
+    @Mock private S3Service s3Service;
+    @Mock private RequestedRepository requestedRepository;
+    @Mock private ThumbNailRepository thumbNailRepository;
+    @Mock private MemberRepository memberRepository;
 
 }
